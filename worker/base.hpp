e9037d31a655657ce060d57d3d289e65c7366e67
kark_li <kark_li@qq.com>
2021-03-31 20:31:27 +0800

    feat: 更新API原型

--- worker/base.hpp
+++ worker/base.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <istream>
 #include <unordered_map>
+#include <atomic>
 #include <NvInfer.h>
 #include "../util/RWMutex/rwmutex.hpp"
 
@@ -12,8 +13,9 @@ enum ModelType
     TRT_ENGINE
 };
 
-typedef struct EngineInfo {
-    nvinfer1::ICudaEngine * engine;
+typedef struct EngineInfo
+{
+    nvinfer1::ICudaEngine *engine;
     std::string InputName;
     std::string OutputName;
     uint32_t InputSize;
@@ -21,6 +23,9 @@ typedef struct EngineInfo {
 } EngineInfo;
 // model_table 全局唯一索引与模型名称对照表
 static std::unordered_map<int, std::string> model_table;
+
+// max_index 当前最大index
+static atomic<int> max_index(-1);
 
 // mt_rw_mu model_table配套RW锁
 static RWMutex mt_rw_mu;
@@ -36,10 +41,11 @@ class IWorker
 public:
     // Load 加载模型到GPU中
     // \param model_name 模型名称，该名称需要与目前已被加载的模型均不同，是一个唯一标识模型的名称
-    // \param model_file 模型输入流，可以为TensorRT引擎或ONNX文件
+    // \param model_file 模型文件，可以为TensorRT引擎或ONNX文件
+    // \param file_path 模型文件的路径
     // \param type 输入流代表的实际类型
     // \returns 该模型在全局的唯一索引，如果导入不成功，将会返回-1，并在logger中输出错误信息
-    virtual int Load(std::string model_name, std::istream model_file, ModelType type) = 0;
+    virtual int Load(std::string model_name, std::string model_file, std::string file_path, ModelType type) = 0;
 
     // UnLoad 从GPU中卸载模型
     // \param model_name 模型名称，该名称是在LoadModel时指定的。
@@ -48,7 +54,7 @@ public:
 
     // GetModelName 获得指定索引的模型的名称
     // \param index 模型在全局的唯一索引
-    // \returns 该模型的名称，如果该模型的索引不存在，将会返回空串，并在logger中输出错误信息
+    // \returns 该模型的名称，如果该模型的索引不存在，将会返回空串
     virtual std::string GetModelName(int index) const = 0;
 
     // Compute 开始根据模型执行计算
