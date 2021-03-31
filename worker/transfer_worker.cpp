e7c60120c8a3012979eb802892754dcc5f3a3417
kark_li <kark_li@qq.com>
2021-03-31 20:30:57 +0800

    feat: 完成transfer_worker的构建

--- worker/transfer_worker.cpp
+++ worker/transfer_worker.cpp
@@ -1,1 +1,162 @@
 #include "transfer_worker.hpp"
+#include "trt_allocator.hpp"
+#include "common.hpp"
+#include <NvOnnxParser.h>
+#include <NvInferRuntimeCommon.h>
+#include "common/logger.h" // On TensorRT/samples
+#include <memory>
+#include <iostream>
+using std::cerr;
+using std::endl;
+using std::string;
+using std::unique_ptr;
+
+
+
+int Load(std::string model_name, std::string model_file, std::string file_path, ModelType type)
+{
+    auto builder = unique_ptr<nvinfer1::IBuilder>(nvinfer1::createInferBuilder(gLogger.getTRTLogger()));
+    if (!builder)
+    {
+        return -1;
+    }
+
+    auto network = unique_ptr<nvinfer1::INetworkDefinition>(builder->createNetwork());
+    if (!network)
+    {
+        return -1;
+    }
+
+    auto config = unique_ptr<nvinfer1::IBuilderConfig>(builder->createBuilderConfig());
+    if (!config)
+    {
+        return -1;
+    }
+
+    switch (type)
+    {
+    case ONNX_FILE:
+    {
+        auto parser = unique_ptr<nvonnxparser::IParser>(nvonnxparser::createParser(*network, gLogger.getTRTLogger()));
+        if (!parser)
+        {
+            return -1;
+        }
+        auto parsed = parser->parseFromFile((file_path + model_file).c_str(), static_cast<int>(gLogger.getReportableSeverity()));
+        if (!parsed)
+        {
+            return -1;
+        }
+        builder->setGpuAllocator(kg_allocator);
+        builder->setMaxBatchSize(1);
+        builder->setMaxWorkspaceSize(32 * (1 << 20));
+        // 默认采取int8精度进行快速推理
+        config->setFlag(nvinfer1::BuilderFlag::kINT8);
+        break;
+    }
+    case TRT_ENGINE:
+#pragma message("error! TensorRT deserilaize is not avaliable now.")
+        gLogError << "error! TensorRT deserilaize is not avaliable now." << endl;
+        return;
+        break;
+    default:
+        break;
+    }
+    // 从网络构建引擎，并保存在engine_table中
+    auto engine = builder->buildEngineWithConfig(*network, *config);
+    if (!engine)
+    {
+        return -1;
+    }
+    // 生成索引
+    mt_rw_mu.lock();
+    max_index++;
+    int current_index = max_index.load();
+    model_table.insert(std::pair<int, std::string>(current_index, model_name));
+    mt_rw_mu.unlock();
+
+    // 获取输入/输出最大空间
+    uint32_t inputMaxSize = 0UL;
+    uint32_t outputMaxSize = 0UL;
+
+    auto inputDims = network->getInput(0)->getDimensions();
+    auto outputDims = network->getOutput(0)->getDimensions();
+
+    // 在batch_size = 1的情况下，通常i不会超过3（CHW），对于灰度图来说，i一般为2（HW）
+    for (int i = 0; i < inputDims.nbDims; i++)
+    {
+        if (inputDims.d[i] == 0)
+            continue;
+        if (inputMaxSize == 0)
+        {
+            inputMaxSize += sizeof(float) * inputDims.d[i];
+        }
+        else
+        {
+            inputMaxSize *= sizeof(float) * inputDims.d[i];
+        }
+    }
+
+    // 在batch_size = 1的情况下，对于image_classfication来说，通常i不会超过3（CHW），对于灰度图来说，i一般为2（HW）
+    for (int i = 0; i < outputDims.nbDims; i++)
+    {
+        if (outputDims.d[i] == 0)
+            continue;
+        if (outputMaxSize == 0)
+        {
+            outputMaxSize += sizeof(float) * outputDims.d[i];
+        }
+        else
+        {
+            outputMaxSize *= sizeof(float) * outputDims.d[i];
+        }
+    }
+
+    // 插入到engine_table
+    et_rw_mu.lock();
+    EngineInfo ef = {
+        .engine = engine,
+        .InputName = network->getInput(0)->getName(),
+        .OutputName = network->getOutput(0)->getName(),
+    };
+    ef.InputSize = inputMaxSize;
+    ef.OutputSize = outputMaxSize;
+    engine_table.insert(std::pair<std::string, EngineInfo>(model_name, ef));
+    et_rw_mu.unlock();
+
+    return current_index;
+}
+int Unload(std::string model_name)
+{
+    // 从engine_table删除该模型的引擎
+    et_rw_mu.lock();
+    auto iter = engine_table.find(model_name);
+    if (iter == engine_table.end())
+        return -1;
+    engine_table.erase(iter);
+    et_rw_mu.unlock();
+
+    // 再从model_table中删除
+    mt_rw_mu.lock();
+    int index = -1;
+    for (auto iter = model_table.begin(); iter != model_table.end(); ++iter)
+    {
+        if (iter->second == model_name)
+        {
+            model_table.erase(iter);
+            break;
+        }
+    }
+    mt_rw_mu.unlock();
+    return index;
+}
+
+std::string GetModelName(int index)
+{
+    mt_rw_mu.rlock();
+    auto iter = model_table.find(index);
+    mt_rw_mu.runlock();
+    if (iter == model_table.end())
+        return "";
+    return iter->second;
+}
--- worker/transfer_worker.hpp
+++ worker/transfer_worker.hpp
@@ -5,12 +5,10 @@
 #include <unordered_map>
 #include <mutex>
 
-static std::mutex models_mu;
-
 class TransferWorker final : public IWorker
 {
 public:
-    int Load(std::string model_name, std::istream model_file, ModelType type);
+    int Load(std::string model_name, std::string model_file, std::string file_path, ModelType type);
     int Unload(std::string model_name);
     std::string GetModelName(int index);
     void *Compute(std::string model_name, void *input)
