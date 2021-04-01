c39b1130186f8f5776a8f55ac8d479450f9724e1
kark_li <kark_li@qq.com>
2021-04-01 22:11:12 +0800

    update demo

--- worker/main.cpp
+++ worker/main.cpp
@@ -1,9 +1,10 @@
 #include "worker.hpp"
-#include <NvInfer.h>
+// #include <NvInfer.h>
 
 int main()
 {
     TransferWorker transfer_worker;
     ComputationWorker computaion_worker;
+    transfer_worker.Load("mnist", "mnist.onnx", "/home/lijiakang/TensorRT-6.0.1.5/data/mnist/",ONNX_FILE);
     return 0;
 }
