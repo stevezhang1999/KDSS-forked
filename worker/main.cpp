63c3180bce88d95d89f8df438f5f1bd9fa93991a
kark_li <kark_li@qq.com>
2021-04-04 19:09:50 +0800

    demo：增加Mnist的demo代码

--- worker/main.cpp
+++ worker/main.cpp
@@ -1,5 +1,6 @@
 #include "worker.hpp"
 #include "common/common.h"
+#include "kgmalloc.hpp"
 // #include <NvInfer.h>
 #include <fstream>
 #include <iostream>
@@ -12,26 +13,44 @@ int main()
     ComputationWorker computation_worker;
     std::vector<uint8_t> fileData(28 * 28 * sizeof(float));
     // int mNumber = rand() % 10;
-    readPGMFile(std::string() + "/home/lijiakang/TensorRT-6.0.1.5/data/mnist/" + std::to_string(6) + ".pgm", fileData.data(), 28, 28);
+    readPGMFile(std::string() + "/home/lijiakang/TensorRT-6.0.1.5/data/mnist/" + std::to_string(1) + ".pgm", fileData.data(), 28, 28);
     auto input_size = 28 * 28;
     float test_data[28 * 28];
     memset(test_data, 0, sizeof(float) * 28 * 28);
-    for (int i = 0; i < 28*28; i++)
+    for (int i = 0; i < 28 * 28; i++)
     {
         test_data[i] = 1.0 - float(fileData[i] / 255.0);
     }
-    transfer_worker.Load("mnist", "mnist.onnx", "/home/lijiakang/TensorRT-6.0.1.5/data/mnist/", ONNX_FILE);
-    // transfer_worker.Load("mnist_standard", "mnist.onnx", "/home/lijiakang/TensorRT-6.0.1.5/data/mnist/", ONNX_FILE, test_data);
-
-    void *h_output = computation_worker.Compute("mnist", test_data);
-    if (!h_output)
+    int loaded = transfer_worker.Load("mnist", "mnist.onnx", "/home/lijiakang/TensorRT-6.0.1.5/data/mnist/", ONNX_FILE);
+    std::vector<std::vector<char>> input;
+    preProcessHostInput(input, test_data, 28 * 28, nvinfer1::DataType::kFLOAT);
+    auto h_output = computation_worker.Compute("mnist", input);
+    if (!h_output.size())
     {
         gLogError << "Invaild output, exit..." << endl;
         return -1;
     }
-    float *output = static_cast<float *>(h_output);
+    // float *output = static_cast<float *>(h_output[0].data());
+    float *output = (float *)new char[h_output[0].size()];
+    if (output)
     {
-        const int outputSize = computation_worker.GetModelOutputDim("mnist")[0];
+        int executed = preProcessHostOutput(h_output, 0, (void **)&output, 10, nvinfer1::DataType::kFLOAT);
+        if (executed)
+        {
+            gLogError << "Process output failed, exit..." << endl;
+            return -1;
+        }
+    }
+    
+    {
+        uint64_t outputSize = 0;
+        int executed = computation_worker.GetModelOutputSize("mnist", 0, &outputSize);
+        if (executed)
+        {
+            gLogError << "Cannot get output size of output[0]" << endl;
+            return -1;
+        }
+        outputSize /= sizeof(float);
         float val{0.0f};
         int idx{0};
 
