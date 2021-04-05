c7eb43d9370ad830e192a29fc3a3c254a7a5548f
kark_li <kark_li@qq.com>
2021-04-05 13:43:42 +0800

    feat: 增加把运行时间写到文件输出流的方法

--- worker/common.hpp
+++ worker/common.hpp
@@ -2,4 +2,19 @@
 
 #define __CXX_PREFIX __FILE__ << ":" << __LINE__ << " "
 
+#if defined __unix || __linux
+#include <sys/time.h>
+#include <iomanip>
+#define _CXX_MEASURE_TIME(expr, ofstream)                                       \
+    do                                                                          \
+    {                                                                           \
+        struct timeval t1, t2;                                                  \
+        gettimeofday(&t1, NULL);                                                \
+        (expr);                                                                 \
+        gettimeofday(&t2, NULL);                                                \
+        double time = (t2.tv_sec - t1.tv_sec) * 1000;                           \
+        time += (t2.tv_usec - t1.tv_usec) * 1.0f / 1000;                        \
+        ofstream << setiosflags(ios::fixed) << setprecision(3) << time << endl; \
+    } while (0)
+#endif
 // end of common.hpp
