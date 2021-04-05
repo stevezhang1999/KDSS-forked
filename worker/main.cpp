18125a251ae9c63cfdf288e05db065a2962e3c40
kark_li <kark_li@qq.com>
2021-04-05 13:45:14 +0800

    demo: 还原demo代码，与performance分离

--- worker/main.cpp
+++ worker/main.cpp
@@ -13,8 +13,8 @@ int main()
     TransferWorker transfer_worker;
     ComputationWorker computation_worker;
     std::vector<uint8_t> fileData(28 * 28 * sizeof(float));
-    // int mNumber = rand() % 10;
-    readPGMFile(std::string() + "/home/lijiakang/TensorRT-6.0.1.5/data/mnist/" + std::to_string(1) + ".pgm", fileData.data(), 28, 28);
+    int mNumber = rand() % 10;
+    readPGMFile(std::string() + "/home/lijiakang/TensorRT-6.0.1.5/data/mnist/" + std::to_string(mNumber) + ".pgm", fileData.data(), 28, 28);
     auto input_size = 28 * 28;
     float test_data[28 * 28];
     memset(test_data, 0, sizeof(float) * 28 * 28);
@@ -23,43 +23,19 @@ int main()
         test_data[i] = 1.0 - float(fileData[i] / 255.0);
     }
     int loaded;
-    // loaded = transfer_worker.Load("mnist", "mnist.onnx", "/home/lijiakang/TensorRT-6.0.1.5/data/mnist/", ONNX_FILE);
-    // if (loaded != 0)
-    // {
-    //     gLogFatal << "Loading mnist model into memory failed." << endl;
-    //     return loaded;
-    // }
-    vector<string> in_vec;
-    in_vec.push_back("Input3");
-    vector<string> out_vec;
-    out_vec.push_back("Plus214_Output_0");
-    loaded = transfer_worker.LoadFromEngineFile("mnist_standard", "mnist.trtengine", "/home/lijiakang/TensorRT-6.0.1.5/data/mnist/", in_vec, out_vec);
-    if (loaded != 0)
+    loaded = transfer_worker.Load("mnist", "mnist.onnx", "/home/lijiakang/TensorRT-6.0.1.5/data/mnist/", ONNX_FILE);
+    if (loaded == -1)
     {
         gLogFatal << "Loading mnist model into memory failed." << endl;
         return loaded;
     }
+
     std::vector<std::vector<char>> input;
     preProcessHostInput(input, test_data, 28 * 28, nvinfer1::DataType::kFLOAT);
     std::vector<std::vector<char>> h_output;
 
-    // Just test
-    h_output = computation_worker.Compute("mnist_standard", input);
+    h_output = computation_worker.Compute("mnist", input);
 
-    // Cold start
-    // h_output = computation_worker.Compute("mnist", input);
-    // h_output = computation_worker.ComputeWithStream("mnist", input);
-
-    // for (int i = 1; i <= 15000; i++)
-    // {
-    //     // cout << endl
-    //     //      << i << " times compute: " << endl;
-    //     // MEASURE_TIME(h_output = computation_worker.Compute("mnist", input));
-    //     // MEASURE_TIME(h_output = computation_worker.ComputeWithStream("mnist", input));
-    //     h_output = computation_worker.Compute("mnist", input);
-    //     h_output = computation_worker.ComputeWithStream("mnist", input);
-    // }
-    // return 0;
     if (!h_output.size())
     {
         gLogError << __CXX_PREFIX << "Invaild output, exit..." << endl;
