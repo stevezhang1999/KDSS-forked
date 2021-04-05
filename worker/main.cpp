21a95f4e340f92d89b8bba52bdbe733cf3d2b4b1
kark_li <kark_li@qq.com>
2021-04-05 11:44:09 +0800

    demo: 更新了demo

--- worker/main.cpp
+++ worker/main.cpp
@@ -1,6 +1,7 @@
 #include "worker.hpp"
 #include "common/common.h"
 #include "kgmalloc.hpp"
+#include "common.hpp"
 // #include <NvInfer.h>
 #include <fstream>
 #include <iostream>
@@ -21,26 +22,47 @@ int main()
     {
         test_data[i] = 1.0 - float(fileData[i] / 255.0);
     }
-    int loaded = transfer_worker.Load("mnist", "mnist.onnx", "/home/lijiakang/TensorRT-6.0.1.5/data/mnist/", ONNX_FILE);
+    int loaded;
+    // loaded = transfer_worker.Load("mnist", "mnist.onnx", "/home/lijiakang/TensorRT-6.0.1.5/data/mnist/", ONNX_FILE);
+    // if (loaded != 0)
+    // {
+    //     gLogFatal << "Loading mnist model into memory failed." << endl;
+    //     return loaded;
+    // }
+    vector<string> in_vec;
+    in_vec.push_back("Input3");
+    vector<string> out_vec;
+    out_vec.push_back("Plus214_Output_0");
+    loaded = transfer_worker.LoadFromEngineFile("mnist_standard", "mnist.trtengine", "/home/lijiakang/TensorRT-6.0.1.5/data/mnist/", in_vec, out_vec);
+    if (loaded != 0)
+    {
+        gLogFatal << "Loading mnist model into memory failed." << endl;
+        return loaded;
+    }
     std::vector<std::vector<char>> input;
     preProcessHostInput(input, test_data, 28 * 28, nvinfer1::DataType::kFLOAT);
     std::vector<std::vector<char>> h_output;
 
-    // Cold start
-    h_output = computation_worker.Compute("mnist", input);
-    h_output = computation_worker.ComputeWithStream("mnist", input);
+    // Just test
+    h_output = computation_worker.Compute("mnist_standard", input);
 
-    for (int i = 0; i < 20; i++)
-    {
-        cout << endl
-             << i << " times compute: " << endl;
-        MEASURE_TIME(h_output = computation_worker.Compute("mnist", input));
-        MEASURE_TIME(h_output = computation_worker.ComputeWithStream("mnist", input));
-    }
-    return 0;
+    // Cold start
+    // h_output = computation_worker.Compute("mnist", input);
+    // h_output = computation_worker.ComputeWithStream("mnist", input);
+
+    // for (int i = 1; i <= 15000; i++)
+    // {
+    //     // cout << endl
+    //     //      << i << " times compute: " << endl;
+    //     // MEASURE_TIME(h_output = computation_worker.Compute("mnist", input));
+    //     // MEASURE_TIME(h_output = computation_worker.ComputeWithStream("mnist", input));
+    //     h_output = computation_worker.Compute("mnist", input);
+    //     h_output = computation_worker.ComputeWithStream("mnist", input);
+    // }
+    // return 0;
     if (!h_output.size())
     {
-        gLogError << "Invaild output, exit..." << endl;
+        gLogError << __CXX_PREFIX << "Invaild output, exit..." << endl;
         return -1;
     }
     // float *output = static_cast<float *>(h_output[0].data());
@@ -50,17 +72,17 @@ int main()
         int executed = preProcessHostOutput(h_output, 0, (void **)&output, 10, nvinfer1::DataType::kFLOAT);
         if (executed)
         {
-            gLogError << "Process output failed, exit..." << endl;
+            gLogError << __CXX_PREFIX << "Process output failed, exit..." << endl;
             return -1;
         }
     }
 
     {
         uint64_t outputSize = 0;
-        int executed = computation_worker.GetModelOutputSize("mnist", 0, &outputSize);
+        int executed = computation_worker.GetModelOutputSize("mnist_standard", 0, &outputSize);
         if (executed)
         {
-            gLogError << "Cannot get output size of output[0]" << endl;
+            gLogError << __CXX_PREFIX << "Cannot get output size of output[0]" << endl;
             return -1;
         }
         outputSize /= sizeof(float);
