9a996b6e69bcd8c039424f029e2d66d2bb8efc3b
kark_li <kark_li@qq.com>
2021-04-14 11:45:26 -0400

    fix: 修复RWMutex的初始化问题及读锁释放问题

--- util/RWMutex/rwmutex.hpp
+++ util/RWMutex/rwmutex.hpp
@@ -10,8 +10,11 @@ class RWMutex
 private:
     mutex write;
     atomic<int> readers;
+    pthread_mutex_t cond_mu;
+    pthread_cond_t cond;
 
 public:
+    RWMutex();
     void lock();
     void unlock();
     void rlock();
